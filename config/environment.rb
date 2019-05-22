3222103bff6db823273c393ef9b8311363ec2923
Yingqi Chen <chanwk18@hotmail.com>
2019-05-22 19:50:42 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,6 @@ Bundler.require
 
 require_relative '../lib/student'
 
-DB = {:conn => SQLite3::Database.new("db/students.db")}
+database_1=SQLite3::Database.new("db/students.db")
+
+DB = {:conn => database_1 }
